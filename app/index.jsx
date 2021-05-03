aa840a8b986a97153c76ba221f70be3e3dd21657
Emir AL <36728456+themythia@users.noreply.github.com>
2021-05-03 17:56:53 +0300

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return <div>finally</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
