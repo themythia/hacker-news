a0627a72ad3a4294c91f402ca87ef80999536d8f
Emir AL <36728456+themythia@users.noreply.github.com>
2021-05-20 13:53:57 +0300

    refactored comment to hooks

--- app/components/Comment.js
+++ app/components/Comment.js
@@ -4,81 +4,72 @@ import { fetchComments } from '../utils/api';
 import Loading from './Loading';
 import Post from './Post';
 import { Link } from 'react-router-dom';
-import { ThemeConsumer, ThemeProvider } from '../contexts/Theme';
+import ThemeContext from '../contexts/Theme';
 
-export default class Comment extends React.Component {
-  state = {
-    post: null,
-    comments: null,
-  };
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search);
-    this.getComments(id);
-  }
+// const { playerOne, playerTwo } = queryString.parse(location.search);
 
-  getComments = (id) => {
+const Comment = ({ location }) => {
+  const { id } = queryString.parse(location.search);
+  const [post, setPost] = React.useState(null);
+  const [comments, setComments] = React.useState(null);
+  const theme = React.useContext(ThemeContext);
+
+  React.useEffect(() => {
     fetchComments(id)
       .then((data) => {
-        this.setState(({ post }) => ({ post: data[0] }));
+        setPost(data[0]);
         return data[1];
       })
-      .then((commentData) =>
-        this.setState(({ comments }) => ({ comments: commentData }))
-      );
-  };
+      .then((commentData) => setComments(commentData))
+      .catch((error) => console.warn(error));
+  });
 
-  getDate = (num) => {
+  const getDate = (num) => {
     const dateObj = new Date(num * 1000);
     return dateObj.toLocaleString();
   };
-  render() {
-    const { comments, post } = this.state;
-    return (
-      <div className='comments-div'>
-        {this.state.comments === null && <Loading text='Fetching Comments' />}
-        {this.state.comments !== null && (
-          <Post
-            title={post.title}
-            url={post.url}
-            username={post.by}
-            date={this.getDate(post.time)}
-            comment={post.descendants}
-            postId={post.id}
-          />
-        )}
-        <ThemeConsumer>
-          {({ theme }) => (
-            <ul>
-              {this.state.comments !== null &&
-                comments.map((comment) => {
-                  return (
-                    <li key={comment.id} className='comment'>
-                      <p className='faded'>
-                        {`by `}
-                        <span>
-                          <Link
-                            className={`link link-${theme}`}
-                            to={{
-                              pathname: '/user',
-                              search: `?id=${comment.by}`,
-                            }}
-                          >
-                            {comment.by}
-                          </Link>
-                        </span>
-                        {` on ${this.getDate(comment.time)}`}
-                      </p>
-                      <p
-                        dangerouslySetInnerHTML={{ __html: comment.text }}
-                        className={`comment-text-${theme}`}
-                      ></p>
-                    </li>
-                  );
-                })}
-            </ul>
-          )}
-        </ThemeConsumer>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='comments-div'>
+      {comments === null && <Loading text='Fetching Comments' />}
+      {comments !== null && (
+        <Post
+          title={post.title}
+          url={post.url}
+          username={post.by}
+          date={getDate(post.time)}
+          comment={post.descendants}
+          postId={post.id}
+        />
+      )}
+      <ul>
+        {comments !== null &&
+          comments.map((comment) => {
+            return (
+              <li key={comment.id} className='comment'>
+                <p className='faded'>
+                  {`by `}
+                  <span>
+                    <Link
+                      className={`link link-${theme}`}
+                      to={{
+                        pathname: '/user',
+                        search: `?id=${comment.by}`,
+                      }}
+                    >
+                      {comment.by}
+                    </Link>
+                  </span>
+                  {` on ${getDate(comment.time)}`}
+                </p>
+                <p
+                  dangerouslySetInnerHTML={{ __html: comment.text }}
+                  className={`comment-text-${theme}`}
+                ></p>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
+export default Comment;
